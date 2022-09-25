6bfd468db9b5225db3fa02ab5671bee9d88d15bd
JR <jose.alonso.programmer@gmail.com>
2022-09-25 19:56:03 +0200

    Animate the background circles

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 export default function BackgroundCircles({ }: Props) {
   return (
-    <div className='relative flex justify-center items-center'>
+    <motion.div
+      initial={{
+        opacity: 0
+      }}
+      animate={{
+        scale: [1, 2, 2, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+        borderRadius: ["20%", "20%", "50%", "80%", "20%",],
+      }}
+      transition={{
+        duration: 2.5,
+      }}
+      className='relative flex justify-center items-center'
+    >
       <div className='absolute border border-[#ffffff] rounded-full h-[200px] w-[200px]
         mt-52 animate-ping'/>
       <div className='rounded-full border border-[#333333] h-[300px] w-[300px]
@@ -15,6 +29,6 @@ export default function BackgroundCircles({ }: Props) {
         absolute mt-52 animate-pulse'/>
       <div className='rounded-full border border-[#333333] h-[800px] w-[800px]
         absolute mt-52'/>
-    </div>
+    </motion.div>
   )
 }
