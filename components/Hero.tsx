f96b19ccfc67bddbf3cfa88e7bbf3c233b5f0fba
JR <jose.alonso.programmer@gmail.com>
2022-09-26 00:22:07 +0200

    Fix the layering issue by which hover wasn't working

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,7 @@ export default function Hero({ }: Props) {
                 src=''
                 alt=''
             />
-            <div>
+            <div className='z-20'>
                 <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
                     Software Engineer
                 </h2>
