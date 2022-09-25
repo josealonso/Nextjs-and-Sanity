089ad79d530e5091c2c08e6f8bb4f17ef5365501
JR <jose.alonso.programmer@gmail.com>
2022-09-26 00:19:14 +0200

    Add buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles';
@@ -17,10 +18,35 @@ export default function Hero({ }: Props) {
         <div className='h-screen flex flex-col space-y-8 items-center justify-center
         text-center overflow-hidden'>
             <BackgroundCircles />
-            <h1>
-                <span>{text}</span>
-                <Cursor cursorColor='yellow' />
-            </h1>
+            <img
+                className='relative rounded-full h-32 w-32 mx-auto object-cover'
+                src=''
+                alt=''
+            />
+            <div>
+                <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+                    Software Engineer
+                </h2>
+                <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                    <span>{text}</span>
+                    <Cursor cursorColor='yellow' />
+                </h1>
+
+                <div className='pt-5'>
+                    <Link href="#about">
+                        <button className='heroButton'>About</button>
+                    </Link>
+                    <Link href="#experience">
+                        <button className='heroButton'>Experience</button>
+                    </Link>
+                    <Link href="#skills">
+                        <button className='heroButton'>Skills</button>
+                    </Link>
+                    <Link href="#project">
+                        <button className='heroButton'>Projects</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
