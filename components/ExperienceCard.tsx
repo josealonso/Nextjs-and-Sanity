8f57eb0fe6c218abf93b41b19a29f54922c87360
JR <jose.alonso.programmer@gmail.com>
2022-09-26 12:41:00 +0200

    Add opacity transition effect on hover

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -6,7 +6,8 @@ type Props = {}
 export default function ExperienceCard({ }: Props) {
     return (
         <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 
-         w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10'>
+         w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10
+         hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
             <motion.img
                 initial={{
                     y: -100,
