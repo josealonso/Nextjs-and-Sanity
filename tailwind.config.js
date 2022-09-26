969150d8cec22bd092775ebebf55da1707e1a307
JR <jose.alonso.programmer@gmail.com>
2022-09-26 12:31:44 +0200

    Build work experience cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './components/Header.tsx',
     './components/BackgroundCircles.tsx',
     './components/Hero.tsx',
     './components/About.tsx',
+    './components/WorkExperience.tsx',
+    './components/ExperienceCard.tsx',
     './pages/**/*.tsx',
     '.components/**/*.tsx',
   ],
