c39e1606e4b08c3f91e12fe7aeb53c5a0ae324fc
JR <jose.alonso.programmer@gmail.com>
2022-09-26 11:16:24 +0200

    Add work experience section

---
+++ components/WorkExperience.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function WorkExperience({ }: Props) {
+    return (
+        <div className='h-screen'>
+            WorkExperience
+        </div>
+    )
+}
