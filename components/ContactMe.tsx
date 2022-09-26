be5c177eae138d2150b595a0ebdd94f6b6bc6c97
JR <jose.alonso.programmer@gmail.com>
2022-09-26 19:11:02 +0200

    Build a form inside the Contact section

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -32,10 +32,26 @@ export default function ContactMe({ }: Props) {
                         <MapPinIcon className='text-[#f7ab0a] h-7 w-7 animate-pulse' />
                         <p className='text-2xl'>Gran Vía, 42, Madrid</p>
                     </div>
-
                 </div>
 
-            </div>
-        </div>
+                <form className='flex flex-col space-y-2 w-fit mx-auto'>
+                    <div className='flex space-x-2'>
+                        <input placeholder='Name' className='contactInput' type="text" />
+                        <input placeholder='Email' className='contactInput' type="email" />
+                    </div>
+
+                    <input placeholder='Subject' className='contactInput' type="text" />
+
+                    <textarea placeholder='Message' className='contactInput' />
+                    <button
+                        type='submit'
+                        className='bg-[#f7ab0a] py-5 px-10 rounded-md text-black font-bold text-lg'
+                    >
+                        Submit
+                    </button>
+                </form>
+
+            </div >
+        </div >
     )
 }
