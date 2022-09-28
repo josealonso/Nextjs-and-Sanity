03f353748042c17a6395179b276f72662f78b5d2
JR <jose.alonso.programmer@gmail.com>
2022-09-29 01:52:30 +0200

    Add Sanity data in the Skills section.

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,11 +1,14 @@
 import { motion } from 'framer-motion'
 import React from 'react'
+import { Skill } from '../typings';
+import { urlFor } from '../sanity';
 
 type Props = {
+    skill: Skill;
     fromLeft?: boolean;
 }
 
-export default function Skill({ fromLeft }: Props) {
+export default function Skill({ skill, fromLeft }: Props) {
     return (
         <div className='group relative flex cursor-pointer'>
             <motion.img
@@ -15,7 +18,7 @@ export default function Skill({ fromLeft }: Props) {
                 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 1 }}
-                src=''
+                src={urlFor(skill?.image).url()}
                 className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28
                 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
             />
@@ -24,7 +27,9 @@ export default function Skill({ fromLeft }: Props) {
             ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32
             rounded-full z-0'>
                 <div className='flex items-center justify-center h-full'>
-                    <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+                    <p className='text-3xl font-bold text-black opacity-100'>
+                        {skill.progress} %
+                    </p>
                 </div>
             </div>
         </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,10 +1,13 @@
 import { motion } from 'framer-motion'
 import React from 'react'
 import Skill from './Skill'
+import { Skill as SkillType } from '../typings'
 
-type Props = {}
+type Props = {
+    skills: SkillType[];
+}
 
-export default function Skills({ }: Props) {
+export default function Skills({ skills }: Props) {
     return (
         <motion.div
             initial={{ opacity: 0 }}
@@ -23,21 +26,14 @@ export default function Skills({ }: Props) {
             </h3>
 
             <div className='grid grid-cols-4 gap-4'>
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
+                {skills?.slice(0, skills.length / 2).map((skill) => (
+                    <Skill key={skill._id} skill={skill} />
+                ))}
+
+                {skills?.slice(skills.length / 2, skills.length).map((skill) => (
+                    <Skill key={skill._id} skill={skill} fromLeft />
+                ))}
+
             </div>
         </motion.div>
     )
