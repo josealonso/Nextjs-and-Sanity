77eed91be7928d1afa5a9fbacb5a9b0d08feaefa
JR <jose.alonso.programmer@gmail.com>
2022-09-28 21:25:11 +0200

    Fix typos

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -3,8 +3,7 @@ import { PageInfo } from "../typings";
 export const fetchPageInfo = async () => {
     console.log(`pageInfo endpoint: 
     ${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
-    // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
-    const res = await fetch(`http://localhost:3000/api/getPageInfo`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
 
     const data = await res.json();
     const pageInfo: PageInfo = data.pageInfo;
